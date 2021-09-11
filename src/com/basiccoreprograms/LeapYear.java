8c8696d1f97165b92f7852cb0ea421a15edb6275
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-11 17:20:25 +0530

    Solved: Leap Year Problem.

--- src/com/basiccoreprograms/LeapYear.java
+++ src/com/basiccoreprograms/LeapYear.java
@@ -1,9 +1,20 @@
 package com.basiccoreprograms;
 
+import java.util.Scanner;
+
 public class LeapYear {
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome To Leap Year Program.");
+		System.out.print("Enter Year To Check It's A Leap Year Or Not: ");
+        Scanner scan = new Scanner(System.in);
+        int year = scan.nextInt();
+        scan.close();
+
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year");
+        } else {
+            System.out.println(year + " is not a leap year");
+        }
 	}
 	
 }
