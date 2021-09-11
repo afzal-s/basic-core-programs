3b2a2780227c01a6f781877ac95e85bd1a094f57
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-11 17:32:09 +0530

    Solved: Prime Factors Problem.

--- src/com/basiccoreprograms/PrimeFactors.java
+++ src/com/basiccoreprograms/PrimeFactors.java
@@ -1,9 +1,28 @@
 package com.basiccoreprograms;
 
+import java.util.Scanner;
+
 public class PrimeFactors {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To Prime Factors Problem.");
+		System.out.print("Enter Number To Find Prime Factors: ");
+        Scanner scanner = new Scanner(System.in);
+        int prime = scanner.nextInt();
+        scanner.close();
+        
+        if (prime == 1) {
+            System.out.println(prime + " is not a Prime Number");
+        } else {
+            for (int i = 2; i * i <= prime; i++) {
+                while (prime % i == 0) {
+                    System.out.println(i);
+                    prime = prime / i;
+                }
+            }
+            if (prime > 2) {
+                System.out.println(prime);
+            }
+        }
 	}
 
 }
