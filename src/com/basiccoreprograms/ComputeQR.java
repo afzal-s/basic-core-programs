d67f2d931edc28d8940a0711145355d518bff8b2
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-11 17:35:37 +0530

    Solved: Computing Quotient & Remainder Problem.

--- src/com/basiccoreprograms/ComputeQR.java
+++ src/com/basiccoreprograms/ComputeQR.java
@@ -1,9 +1,22 @@
 package com.basiccoreprograms;
 
+import java.util.Scanner;
+
 public class ComputeQR {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To Compute Quotient and Remainder Program.");
+		Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the value of the Divisor: ");
+        int divisor = scan.nextInt();
+        System.out.print("Enter the value of the Dividend: ");
+        int divident = scan.nextInt();
+        scan.close();
+
+        int quotient = divident / divisor;
+        int remainder = divident % divisor;
+
+        System.out.println("Quotient value:" + quotient);
+        System.out.println("Remainder value:" + remainder);
 	}
 
 }
