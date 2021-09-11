a37be694c8abdb09ae7b79c398b059ef19e9fc6c
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-11 17:28:06 +0530

    Solved: To Print The Nth Harmonic Numbers.

--- src/com/basiccoreprograms/HarmonicNumber.java
+++ src/com/basiccoreprograms/HarmonicNumber.java
@@ -1,10 +1,23 @@
 package com.basiccoreprograms;
 
+import java.util.Scanner;
+
 public class HarmonicNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("Enter the Nth number of Harmonic: ");
+        Scanner scan = new Scanner(System.in);
+        int harmonics = scan.nextInt();
+        scan.close();
+        
+        double sum = 0.0;
+        
+        for (int count = 1; count <= harmonics; count++) {
+            System.out.print("1/" + count + " + ");
+            sum += 1.0 / count;
+        }
+        
+        System.out.println("The Nth Harmonic Value: " + sum);
 	}
 
 }
