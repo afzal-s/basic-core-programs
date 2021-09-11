adb99ecada0743ac2ab47ce7b004cf24977288a1
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-11 17:24:09 +0530

    Solved: Power Of Two Problem.

--- src/com/basiccoreprograms/PowerOfTwo.java
+++ src/com/basiccoreprograms/PowerOfTwo.java
@@ -1,9 +1,20 @@
 package com.basiccoreprograms;
 
+import java.util.Scanner;
+
 public class PowerOfTwo {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To Power Of Two Program.");
+		int num = 1;
+        System.out.print("Enter a number to print table power of 2: ");
+        Scanner scan = new Scanner(System.in);
+        Long inpNum = scan.nextLong();
+        scan.close();
+        
+        for (int i = 1; i<=inpNum; i++) {
+            num = num * 2;
+            System.out.println("2 ^ " + i + " = " + num);
+        }
 	}
 
 }
