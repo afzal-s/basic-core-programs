bf9a79b0a40662d6a3530aa802d9bfa0fed56ce5
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-14 21:53:54 +0530

    Solved: Given Input Alphabet Is Vowel Or Consonant.

--- src/com/basiccoreprograms/VowelConsonant.java
+++ src/com/basiccoreprograms/VowelConsonant.java
@@ -1,9 +1,19 @@
 package com.basiccoreprograms;
 
+import java.util.Scanner;
+
 public class VowelConsonant {
-
 	public static void main(String[] args) {
-		System.out.println("Welcome To Vowel & Consonant Program.");
-	}
+		System.out.print("Enter a character :");
+		Scanner scan = new Scanner(System.in);
+		char alphabet = scan.next().charAt(0);
+		scan.close();
 
+		if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u'
+				|| alphabet == 'A' || alphabet == 'E' || alphabet == 'I' || alphabet == 'O' || alphabet == 'U') {
+			System.out.println("Given character is an vowel");
+		} else {
+			System.out.println("Given character is a consonant");
+		}
+	}
 }
