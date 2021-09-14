825efa1dbd4cbd468d9dade20fa04bc36ad4be65
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-14 21:48:32 +0530

    Solved: Swapping of two numbers.

--- src/com/basiccoreprograms/SwapTwoNumbers.java
+++ src/com/basiccoreprograms/SwapTwoNumbers.java
@@ -1,9 +1,24 @@
 package com.basiccoreprograms;
 
-public class SwapTwoNumbers {
+import java.util.*;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome To Swap Two Number Programs.");
-	}
+public class SwapTwoNumbers {  
+    
+	public static void main(String[] args) {  
+       int num1, num2, temp;
+       Scanner scan = new Scanner(System.in);  
+       System.out.print("Enter value of first number: ");  
+       num1 = scan.nextInt();  
+       System.out.print("Enter value of second number: ");  
+       num2 = scan.nextInt();  
+       scan.close();
+       
+       System.out.println("Before swapping two numbers: " + num1 + " " + num2);   
 
-}
+       temp = num1;  
+       num1 = num2;  
+       num2 = temp;   
+       System.out.println("After swapping: "+ num1 + " " + num2);  
+       System.out.println( );  
+    }    
+}  
