938baa34b365ff2f2d600b7c557b1395557c2dcf
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-14 21:51:09 +0530

    Solved: Even & Odd Number Problem.

--- src/com/basiccoreprograms/EvenOdd.java
+++ src/com/basiccoreprograms/EvenOdd.java
@@ -1,9 +1,18 @@
 package com.basiccoreprograms;
 
+import java.util.Scanner;
+
 public class EvenOdd {
-
 	public static void main(String[] args) {
-		System.out.println("Welcome To Check Even & Odd Number Program.");
-	}
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter Number To Check Even Or Odd: ");
+		int num = scan.nextInt();
+		scan.close();
 
+		if (num % 2 == 0) {
+			System.out.println(num + " Number Is An Even number");
+		} else {
+			System.out.println(num + " Number Is An Odd Number");
+		}
+	}
 }
